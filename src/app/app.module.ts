739f03870585c098bb1b1ac2867c3ef355b40c89
Antoine-Goncalves <goncalves.devweb@gmail.com>
2020-01-03 11:41:57 +0100

    :label: import FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { FirstComponentComponent } from './first-component/first-component.component';
-import { AppareilComponent } from './appareil/appareil.component';
+import { AppComponent } from "./app.component";
+import { FirstComponentComponent } from "./first-component/first-component.component";
+import { AppareilComponent } from "./appareil/appareil.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FirstComponentComponent,
-    AppareilComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, FirstComponentComponent, AppareilComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
